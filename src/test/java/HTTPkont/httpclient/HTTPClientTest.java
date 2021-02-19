552ab8e8f969951d1d46dac6d84f14288195dfe5
Julian pettersen <darlion2301@hotmail.com>
2021-02-19 22:22:55 +0100

    AddedTest

---
+++ src/test/java/HTTPkont/httpclient/HTTPClientTest.java
@@ -0,0 +1,13 @@
+package HTTPkont.httpclient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HTTPClientTest {
+
+    @Test
+    void should_Work() {
+        assertEquals(4, 2+2);
+    }
+}
